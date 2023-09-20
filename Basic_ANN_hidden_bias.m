931d9999e419ba387277f69c0b1838385740d9c2
Dr. Venkataramana Veeramsetty <53935611+vvrgit@users.noreply.github.com>
2023-09-21 05:25:52 +0530

    Basic ANN + hidden Layer + Bias + Day3
    Linear Activation Function

---
+++ Basic_ANN_hidden_bias.m
@@ -0,0 +1,67 @@
+clc
+clear all
+%================================================================%
+%            Import Data                                         %
+%================================================================%
+filename='SampleData_02';
+data=xlsread(filename);
+%================================================================%
+%            Initialization                                      %
+%================================================================%
+eta=0.001;
+lowerlimit=-10;
+upperlimit=10;
+wij=lowerlimit+rand(3,5)*(upperlimit-lowerlimit);
+wjk=lowerlimit+rand(5,1)*(upperlimit-lowerlimit);
+bj=lowerlimit+rand(5,1)*(upperlimit-lowerlimit);
+bk=lowerlimit+rand*(upperlimit-lowerlimit);
+
+%================================================================%
+%            Training with GDA                                   %
+%================================================================%
+for i=1:100000
+    for j=1:size(data,1)
+        x=data(j,1:3);
+        oh=x*wij+bj';
+        ok=oh*wjk+bk;
+        delwjk=eta*(data(j,4)-ok)*oh';
+        delbk=eta*(data(j,4)-ok);
+        wjk=wjk+delwjk;
+        bk=bk+delbk;
+        
+        del_wij=zeros(3,5);
+        delbj=zeros(5,1);
+        for h=1:5
+            del_wij(:,h)=eta*(data(j,4)-ok)*wjk(h,1)*x';
+            delbj(h,1)=eta*(data(j,4)-ok)*wjk(h,1);
+        end
+        wij=wij+del_wij;
+        bj=bj+delbj;
+        
+    end
+end
+
+%================================================================%
+%            Error Calc.                                         %
+%================================================================%
+error=0;
+for j=1:size(data,1)
+        x=data(j,1:3);
+        oh=x*wij+bj';
+        ok=oh*wjk+bk;
+        error=error+abs(ok-data(j,4));
+end
+fprintf('Mean Absolute Error=%f\n',error/size(data,1));
+%================================================================%
+%            Predictions                                         %
+%================================================================%
+x1=input('enter x1 value');
+x2=input('enter x2 value');
+x3=input('enter x3 value');
+x=[x1,x2,x3];
+oh=x*wij;
+ok=oh*wjk;
+fprintf('predicted output=%f',ok);
+%================================================================%
+%            End                                                 %
+%================================================================%
